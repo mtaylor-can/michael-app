d67beb1cfb320ca2d14d6bc8f0875b8d1030a250
Michael Taylor <mtaylor@videotron.ca>
2026-04-21 12:22:16 -0400

    Remove manifest import causing Vercel build error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "../public/manifest.webmanifest";
 import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
