feec5e26415bc1510986c7b117b1d15a65c112a1
Michael Taylor <mtaylor@videotron.ca>
2026-04-21 12:41:28 -0400

    Fix: correct Layout import case

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Values from "./pages/Values";
